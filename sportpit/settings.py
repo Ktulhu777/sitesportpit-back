038b91c0ef6d7428eaa822c614a0a25c56160704
VKalaitanov <kalaytanov93@gmail.com>
2024-03-24 13:38:25 +0100

    test 1

--- sportpit/settings.py
+++ sportpit/settings.py
@@ -94,34 +94,35 @@ WSGI_APPLICATION = 'sportpit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sitesportpit_db',
-        'USER': 'sitesportpit',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'POST': 8080,
-    }
-}
-
-# проводим тест))
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': "django.db.backends.postgresql",
-#         'NAME': 'sitewomen_db',
-#         'USER': 'sitewomen',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sitesportpit_db',
+#         'USER': 'sitesportpit',
 #         'PASSWORD': '1234',
 #         'HOST': 'localhost',
-#         'PORT': 5433,
+#         'POST': 8080,
 #     }
-#
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.sqlite3',
-#     #     'NAME': BASE_DIR / 'db.sqlite3',
-#     # }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': "django.db.backends.postgresql",
+        'NAME': 'sitewomen_db',
+        'USER': 'sitewomen',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': 5433,
+    }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+}
+
+# test commit in main-test
 
 # DATABASES = {
 #     'default': {
